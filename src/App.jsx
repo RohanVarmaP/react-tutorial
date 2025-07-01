9f8b75c136e85f78972a1f7cf11c967580129d5d
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-01 16:20:48 +0530

    challenge 6

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function MainPage() {
+  return (
+    <main>
+      <h1>Fun facts about react</h1>
+      <ol>
+        <li>React is the most popular library.</li>
+        <li>It helps in creases chances for getting jobs</li>
+        <li>React is created and released on 2013</li>
+        <li>Created by johe</li>
+        <li>mainted by meta</li>
+      </ol>
+    </main>
+  )
+}
 
+function HeaderPage() {
+  return (
+    <header>
+      <img src={reactLogo} />
+    </header>
+  )
+}
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HeaderPage />
+      <MainPage />
     </>
   )
 }
