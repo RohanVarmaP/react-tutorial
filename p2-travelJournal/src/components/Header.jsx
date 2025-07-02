aeb8a09c9f499050ae523df72dd9c64daa86819c
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-02 10:52:32 +0530

    challenge 1 for project 2

---
+++ p2-travelJournal/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <img src="\src\assets\Globe.png" alt="Globe image" />
+            <h1>my travel journal</h1>
+
+        </header>
+    )
+}
+
+export default Header
