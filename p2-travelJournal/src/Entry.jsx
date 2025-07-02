34a8b687745e27726b9f082490ab86443eb10192
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-02 12:32:51 +0530

    changing all the datat in main

---
+++ p2-travelJournal/src/Entry.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Entry = (props) => {
+    console.log('hello')
+    console.log(props)
+    return (
+        <article className='journal-entry'>
+            <div className='image-container'>
+                <img src={props.img} alt={props.location} />
+            </div>
+            <div>
+                <img className='Marker-png' src="/src/assets/Marker.png" alt="Markar icon" />
+                <span className='span-country'>Japan</span>
+                <a href={props.link}>View on Google Maps</a>
+                <h2>{props.location}</h2>
+                <p>{props.date}</p>
+                <p>{props.desc}</p>
+            </div>
+        </article >
+    )
+}
+
+export default Entry
