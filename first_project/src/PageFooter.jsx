768a909d524d57f91b5f9d66d25dbf9218eadad2
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-02 09:43:20 +0530

    final commit of first project

---
+++ first_project/src/PageFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageFooter = () => {
+    return (
+        <footer>
+            <small>© 2025 Rv development. All rights reserved</small>
+        </footer>
+    )
+}
+
+export default PageFooter
