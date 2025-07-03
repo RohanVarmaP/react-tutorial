d0cd24d428ea0930e120e70773d96d6db7cd3769
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-03 10:14:43 +0530

    first commit for today. for backtracking

--- p3-chefClaude/src/components/Form.jsx
+++ p3-chefClaude/src/components/Form.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 
-const ingredients = ['chilli', 'pepper', 'oregano', 'chicken', 'tamato']
+// const ingredients = ['chilli', 'pepper', 'oregano', 'chicken', 'tamato']
 
-const ingredientsListItems = ingredients.map((ingredient) => <li key={ingredient}>{ingredient}</li>)
+// const ingredientsListItems = ingredients.map((ingredient) => <li key={ingredient}>{ingredient}</li>)
 
-function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget)
-    const newIngredient = formData.get('ingredient')
-    console.log(newIngredient);
-    ingredients.push(newIngredient)
-    console.log(ingredients)
-}
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const formData = new FormData(event.currentTarget)
+//     const newIngredient = formData.get('ingredient')
+//     console.log(newIngredient);
+//     ingredients.push(newIngredient)
+//     console.log(ingredients)
+// }
 
 
 const Form = () => {
+    const [ingredients, setIngredients] = React.useState(['chilli', 'pepper', 'oregano', 'chicken', 'tamato'])
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const ingredientData = formData.get('ingredient')
+        setIngredients(prevList => prevList.push(ingredientData))
+        console.log(ingredients)
+    }
+
+    function IngredientsList() {
+        return ingredients.map(value => <li key={value}>{value}</li>)
+    }
     return (
         <>
             <form onSubmit={handleSubmit} className='add-ingredient'>
@@ -24,7 +37,7 @@ const Form = () => {
             <div className='ingredent-list'>
                 <h2>Ingredients on hand:</h2>
                 <ul>
-                    {ingredientsListItems}
+                    <IngredientsList />
                 </ul>
             </div>
         </>
