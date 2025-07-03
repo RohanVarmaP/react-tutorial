25eea4526afb47d922a682ca78525629cdb30c0b
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-03 12:43:32 +0530

    using ternary for showing ingredients if there is no ingredients in the list

--- p3-chefClaude/src/components/Form.jsx
+++ p3-chefClaude/src/components/Form.jsx
@@ -4,12 +4,18 @@ import React from 'react'
 const Form = () => {
     const [ingredients, setIngredients] = React.useState(['chilli', 'pepper', 'oregano', 'chicken', 'tamato'])
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
-        const ingredientData = formData.get('ingredient')
-        setIngredients(prevList => [...prevList, ingredientData])
-        console.log(ingredients)
+    // function handleSubmit(event) {
+    //     console.log(event)
+    //     event.preventDefault()
+    //     const formData = new FormData(event.currentTarget)
+    //     const ingredientData = formData.get('ingredient')
+    //     ingredientData ? setIngredients(prevList => [...prevList, ingredientData]) : console.log('Enter a value to add ingredient.')
+    //     console.log(ingredients)
+    // }
+    function addIngredient(FormData) {
+        const ingredientData = FormData.get('ingredient')
+        ingredientData ? setIngredients(prevList => [...prevList, ingredientData]) : console.log('Enter a value to add ingredient.')
+
     }
 
     function IngredientsList() {
@@ -18,16 +24,18 @@ const Form = () => {
 
     return (
         <>
-            <form onSubmit={handleSubmit} className='add-ingredient'>
+            <form action={addIngredient} /*onSubmit={handleSubmit}*/ className='add-ingredient'>
                 <input type="text" placeholder='e.g. oregano' aria-label='Add ingredient' name='ingredient' />
                 <button>+ Add ingredient</button>
             </form>
-            <div className='ingredent-list'>
-                <h2>Ingredients on hand:</h2>
-                <ul>
-                    <IngredientsList />
-                </ul>
-            </div>
+            {ingredients.length ?
+                <div className='ingredent-list'>
+                    <h2>Ingredients on hand:</h2>
+                    <ul>
+                        <IngredientsList />
+                    </ul>
+                </div>
+                : console.log('Nothing in ingredients List.')}
         </>
     )
 }
