4d7d8af3c9e296c6cc9303075f69d9579c37e5f5
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-03 14:57:38 +0530

    created a ready for recipe file for rendering recipe section

--- p3-chefClaude/src/components/Form.jsx
+++ p3-chefClaude/src/components/Form.jsx
@@ -8,19 +8,7 @@ const Form = () => {
         return ingredients.map(value => <li key={value}>{value}</li>)
     }
 
-    function ReadyForRecipe() {
 
-        return (
-            <section className='ready-for-recipe'>
-                <div className='get-recipe-div'>
-                    <h2>Ready for a recipe</h2>
-                    <p>generate a recipe from your list of ingredients</p>
-                </div>
-                <div>
-                    <button onClick={GetRecipe} className='get-recipe'>Get a recipe</button>
-                </div>
-            </section>)
-    }
 
     function Recipe() {
         return (
---
+++ p3-chefClaude/src/components/ReadyForRecipe.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export function ReadyForRecipe() {
+    return (
+        <section className='ready-for-recipe'>
+            <div className='get-recipe-div'>
+                <h2>Ready for a recipe</h2>
+                <p>generate a recipe from your list of ingredients</p>
+            </div>
+            <div>
+                <button onClick={GetRecipe} className='get-recipe'>Get a recipe</button>
+            </div>
+        </section>)
+}
