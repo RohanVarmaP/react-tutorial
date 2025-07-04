a73e4d8d8ff87b054442659f30c92608f738ec38
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-04 14:58:14 +0530

    made s=changes so that topText and bottomText to state and made it dynamically change value in image and inout blocks

--- p4-meme-generator/src/components/Main.jsx
+++ p4-meme-generator/src/components/Main.jsx
@@ -1,28 +1,44 @@
+import { useState } from "react"
+
 export default function Main() {
+    const [meme, setMeme] = useState({
+        topText: 'One does not simply',
+        bottomText: 'Walk into Mordor',
+        imageURL: 'http://i.imgflip.com/1bij.jpg'
+    })
+
+    function handleChange(event) {
+        const name = event.currentTarget.name
+        const value = event.currentTarget.value
+        setMeme(prev => ({ ...prev, [name]: value }))
+        console.log(meme)
+    }
     return (
         <main className="main">
-            <div className="form">
+            <form className="form">
                 <label>Top Text
                     <input
                         type="text"
-                        placeholder="One does not simply"
+                        value={meme.topText}
                         name="topText"
+                        onChange={handleChange}
                     />
                 </label>
 
                 <label>Bottom Text
                     <input
                         type="text"
-                        placeholder="Walk into Mordor"
+                        value={meme.bottomText}
                         name="bottomText"
+                        onChange={handleChange}
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
-            </div>
+            </form>
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" alt="generated Meme" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <img src={meme.imageURL} alt="generated Meme" />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main >
     )
