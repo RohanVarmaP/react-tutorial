1fd14b1e6ec875999a6ace5d91078e94c086359c
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-04 11:28:12 +0530

    now its working with css and used npm markdown to change markdown to html text

--- p3-chefClaude/src/App.jsx
+++ p3-chefClaude/src/App.jsx
@@ -10,9 +10,9 @@ import Recipe from './components/Recipe'
 import { getRecipeFromMistral } from './ai'
 
 function App() {
-  const [ingredients, setIngredients] = React.useState(['chilli', 'pepper', 'oregano', 'chicken', 'tamato'])
+  const [ingredients, setIngredients] = React.useState([])
 
-  const [recipeShown, setRecipeShown] = React.useState(false)
+  const [recipe, setRecipe] = React.useState()
 
   function addIngredient(ingredientData) {
     console.log("in app")
@@ -21,8 +21,10 @@ function App() {
 
   }
 
-  function getRecipe() {
-    setRecipeShown(prevvalue => !prevvalue)
+  async function getRecipe() {
+    const newrecipe = await getRecipeFromMistral(ingredients)
+    console.log(newrecipe)
+    setRecipe(newrecipe)
   }
 
 
@@ -33,7 +35,7 @@ function App() {
         <Form addIngredient={addIngredient} />
         {ingredients.length ? <IngredientList ingredients={ingredients} /> : null}
         {ingredients.length > 4 ? <ReadyForRecipe getRecipe={getRecipe} /> : null}
-        {recipeShown ? <Recipe /> : null}
+        {recipe ? <Recipe recipe={recipe} /> : null}
       </main>
     </>
   )
