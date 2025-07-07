a3c2dbaa59b83f72c20ed15e5eb25d7f75676d30
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-07 12:31:38 +0530

    added randomto get random values for dice

--- p5-tenzies/src/App.js
+++ p5-tenzies/src/App.js
@@ -3,9 +3,25 @@ import './App.css';
 import Die from './components/Die';
 
 function App() {
+
+  function Getrandom() {
+    const randomList = [];
+    for (let i = 0; i < 10; i++) {
+      const randomValue = Math.floor(Math.random() * 6) + 1
+      randomList.push(randomValue)
+    }
+    console.log(randomList)
+    return randomList
+  }
+
   return (
     <main>
       <section className='die-section'>
+        {
+          Getrandom(randomList).map((val) => {
+            <Die value={val} />
+          })
+        }
         <Die />
         <Die />
         <Die />
