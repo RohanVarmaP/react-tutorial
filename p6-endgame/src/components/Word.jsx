d6319f13a7eff1f46961469e43688625dcc9285c
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-08 13:55:34 +0530

    final commit for project-6 final endgame

--- p6-endgame/src/components/Word.jsx
+++ p6-endgame/src/components/Word.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Word = (props) => {
-    const letterElements = props.currentWord.split('').map((letter, index) => <span key={index}>{(props.letterGuessed.includes(letter) || props.isGameOver) ? letter.toUpperCase() : ''}</span>)
+    const letterElements = props.currentWord.split('').map((letter, index) => {
+        const isGuessed = props.letterGuessed.includes(letter)
+        const shouldShow = isGuessed || props.isGameOver
+        const styles = {
+            color: isGuessed ? '#F9F4DA' : props.isGameOver ? '#EC5D49' : 'black',
+        }
+        return (<span key={index} style={styles}>{shouldShow ? letter.toUpperCase() : ''}</span>)
+    })
     return (
         <section className='word-section'>
             {letterElements}
