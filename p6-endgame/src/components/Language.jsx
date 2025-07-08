2bbb3ac0fab805ac0565bbc7f7e143b81abb2768
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-08 10:47:28 +0530

    added a function so for each wrong guess it will strick one language in webpage

--- p6-endgame/src/components/Language.jsx
+++ p6-endgame/src/components/Language.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { languages } from '../data/languages'
+import clsx from 'clsx'
 
-const Language = () => {
+const Language = (props) => {
     function getLanguages(languages) {
-        const languageElement = languages.map((val) => {
+        const languageElement = languages.map((val, index) => {
             const styles = {
                 backgroundColor: val.backgroundColor,
                 color: val.color
             }
+            const className = clsx({
+                lost: props.wrongGuessCount > index
+            })
             return <span
-                className='chip lost'
+                className={`chip ${className}`}
                 key={val.name}
                 style={styles}>
                 {val.name}
